8c064eb6e639cef68c8c24e89ac973728615215b
ahmedaldahshoury <ahmed.aldahshoury@gmail.com>
2017-03-05 23:18:54 +0200

    proc

--- database/user_procs.sql
+++ database/user_procs.sql
@@ -1,25 +1,20 @@
 CREATE OR REPLACE FUNCTION sign_up(email VARCHAR, hashed_password VARCHAR)
 RETURNS BOOLEAN AS $$  --Delimiter for functions and strings
-DECLARE followers INTEGER;
-        conv      INTEGER;
-        conv_id   INTEGER;
-        userID    INTEGER;
+DECLARE userID    INTEGER;
+        creation_time DATETIME;
+        last_login DATETIME;
 BEGIN
-    INSERT user_id
-    INTO Member
-    VALUES ( )
+INSERT INTO Member (member_id, email, password_hash, creation_time, last_login)
+    VALUES (userID, email, hashed_password, creation_time, last_login)
 END; $$
 LANGUAGE PLPGSQL;
 
-CREATE OR REPLACE FUNCTION sign_up(email VARCHAR, hashed_password VARCHAR)
+CREATE OR REPLACE FUNCTION login(member_id VARCHAR)
 RETURNS BOOLEAN AS $$  --Delimiter for functions and strings
-DECLARE followers INTEGER;
-        conv      INTEGER;
-        conv_id   INTEGER;
-        userID    INTEGER;
 BEGIN
-    INSERT user_id
+    UPDATE last_login
     INTO Member
-    VALUES ( )
+    VALUES (now() :: TIMESTAMP)
+    WHERE member_id = member_id
 END; $$
 LANGUAGE PLPGSQL;
