7d92c1aafc9f4cf767a840036d4097dc7eb8c25d
ahmedaldahshoury <ahmed.aldahshoury@gmail.com>
2017-03-05 23:59:48 +0200

    proc modifications

--- database/user_procs.sql
+++ database/user_procs.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE FUNCTION sign_up(email VARCHAR, hashed_password VARCHAR)
 RETURNS BOOLEAN AS $$  --Delimiter for functions and strings
 DECLARE userID    INTEGER;
-        creation_time DATETIME;
-        last_login DATETIME;
+        creation_time CURRENT_TIMESTAMP;
+        last_login CURRENT_TIMESTAMP  ;
 BEGIN
 INSERT INTO Member (member_id, email, password_hash, creation_time, last_login)
     VALUES (userID, email, hashed_password, creation_time, last_login)
@@ -15,6 +15,29 @@ BEGIN
     UPDATE last_login
     INTO Member
     VALUES (now() :: TIMESTAMP)
-    WHERE member_id = member_id
+    WHERE id = member_id
+END; $$
+LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE FUNCTION edit_user(member_id VARCHAR,
+                                       email VARCHAR,
+                                       password VARCHAR,
+                                       first_name VARCHAR,
+                                       last_name VARCHAR,
+                                       date_of_birth DATE,
+                                       )
+    RETURNS SETOF users AS $$
+BEGIN
+    INSERT INTO Member(email, password_hash, first_name, last_name, date_of_birth)
+    VALUES (email, password, first_name, last_name, date_of_birth)
+    WHERE id = member_id
+END; $$
+LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE FUNCTION delete_user(member_id VARCHAR)
+    RETURNS VOID AS $$
+BEGIN
+    DELETE FROM Member
+    WHERE id = member_id
 END; $$
 LANGUAGE PLPGSQL;
