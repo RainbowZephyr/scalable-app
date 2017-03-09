06f17938f8f9db327b72c2d167d81eac26ec02fb
RainbowZephyr <darkfeathers9@gmail.com>
2017-03-09 16:06:09 +0200

    finalized SQL

--- database/schema.sql
+++ database/schema.sql
@@ -1,346 +1,347 @@
 DROP TABLE IF EXISTS member CASCADE;
 CREATE TABLE member (
-    id serial PRIMARY KEY,
-    email varchar UNIQUE NOT NULL,
-    password_hash varchar NOT NULL,
-    first_name varchar NOT NULL,
-    last_name varchar NOT NULL,
-    date_of_birth date,
-    created_at TIMESTAMP
+  id            SERIAL PRIMARY KEY ON DELETE CASCADE,
+  email         VARCHAR UNIQUE NOT NULL,
+  password_hash VARCHAR        NOT NULL,
+  first_name    VARCHAR        NOT NULL,
+  last_name     VARCHAR        NOT NULL,
+  date_of_birth DATE,
+  created_at    TIMESTAMP
 );
 
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
-    user_id int PRIMARY KEY,
-    FOREIGN KEY(user_id) references member(id)
+  user_id INT PRIMARY KEY,
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS admin CASCADE;
 CREATE TABLE admin (
-    admin_id int PRIMARY KEY,
-    FOREIGN KEY(admin_id) references member(id)
+  admin_id INT PRIMARY KEY,
+  FOREIGN KEY (admin_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS sessions CASCADE;
 CREATE TABLE IF NOT EXISTS sessions (
-    id                  serial PRIMARY KEY,
-    user_id             INT NOT NULL,
-    session_start       TIMESTAMP NOT NULL,
-    last_access         TIMESTAMP NOT NULL,
-	address     		VARCHAR,
-    FOREIGN KEY(user_id) REFERENCES member(ID)
+  id            SERIAL PRIMARY KEY,
+  user_id       INT       NOT NULL,
+  session_start TIMESTAMP NOT NULL,
+  last_access   TIMESTAMP NOT NULL,
+  address       VARCHAR,
+  FOREIGN KEY (user_id) REFERENCES member (ID)
 );
 
 DROP TABLE IF EXISTS logins CASCADE;
 CREATE TABLE IF NOT EXISTS logins (
-    id                  serial PRIMARY KEY,
-    user_id             INT NOT NULL,
-    last_login          TIMESTAMP NOT NULL,
-	address     		VARCHAR,
-    logout              BOOLEAN,    
-    FOREIGN KEY(user_id) REFERENCES member(id) 	
+  id         SERIAL PRIMARY KEY,
+  user_id    INT       NOT NULL,
+  last_login TIMESTAMP NOT NULL,
+  address    VARCHAR,
+  logout     BOOLEAN,
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS login_failures CASCADE;
 CREATE TABLE IF NOT EXISTS login_failures (
-	id     	        serial PRIMARY KEY, 
-    user_id          INT NOT NULL,    
-    login_attempt     TIMESTAMP NOT NULL,
-    address         VARCHAR,
-	FOREIGN KEY(userID) REFERENCES member(id) 
+  id            SERIAL PRIMARY KEY,
+  user_id       INT       NOT NULL,
+  login_attempt TIMESTAMP NOT NULL,
+  address       VARCHAR,
+  FOREIGN KEY (userID) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS friends CASCADE;
 CREATE TABLE friends (
-    user_1 int,
-    user_2 int,
-    accepted boolean,
-    PRIMARY KEY(user_1,user_2),
-    FOREIGN KEY(user_1) references member(id),
-    FOREIGN KEY(user_2) references member(id)
+  user_1   INT,
+  user_2   INT,
+  accepted BOOLEAN,
+  PRIMARY KEY (user_1, user_2),
+  FOREIGN KEY (user_1) REFERENCES member (id),
+  FOREIGN KEY (user_2) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS blocks CASCADE;
 CREATE TABLE blocks (
-    user_1 int,
-    user_2 int,
-    PRIMARY KEY (user_1,user_2),
-    FOREIGN KEY (user_1) references member(id),
-    FOREIGN KEY (user_2) references member(id)
+  user_1 INT,
+  user_2 INT,
+  PRIMARY KEY (user_1, user_2),
+  FOREIGN KEY (user_1) REFERENCES member (id),
+  FOREIGN KEY (user_2) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS post CASCADE;
 CREATE TABLE post (
-    id SERIAL PRIMARY KEY,
-    user1_id int,
-    user2_id int,
-    post_text varchar NOT NULL,
-    created_at TIMESTAMP,
-    update_time TIMESTAMP,
-    FOREIGN KEY (user1_id) references member(id),
-    FOREIGN KEY (user2_id) references member(id)
+  id          SERIAL PRIMARY KEY,
+  user1_id    INT,
+  user2_id    INT,
+  post_text   VARCHAR NOT NULL,
+  created_at  TIMESTAMP,
+  update_time TIMESTAMP,
+  FOREIGN KEY (user1_id) REFERENCES member (id),
+  FOREIGN KEY (user2_id) REFERENCES member (id)
 
 );
 
 DROP TABLE IF EXISTS post_tags CASCADE;
 CREATE TABLE post_tags (
-    post_id int,
-    user_id int,
-    PRIMARY KEY (post_id,user_id),
-    FOREIGN Key (post_id) references post(id),
-    FOREIGN Key (user_id) references member(id)
+  post_id INT,
+  user_id INT,
+  PRIMARY KEY (post_id, user_id),
+  FOREIGN KEY (post_id) REFERENCES post (id),
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS post_hashtags CASCADE;
 CREATE TABLE post_hashtags (
-    post_id int,
-    user_id int,
-    hashtag varchar,
-    PRIMARY KEY (post_id, user_id,hashtag),
-    FOREIGN Key (post_id) references post(id),
-    FOREIGN Key (user_id) references member(id)
+  post_id INT,
+  user_id INT,
+  hashtag VARCHAR,
+  PRIMARY KEY (post_id, user_id, hashtag),
+  FOREIGN KEY (post_id) REFERENCES post (id),
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS post_reactions CASCADE;
 CREATE TABLE post_reactions (
-    post_id int,
-    user_id int,
-    reaction_type int,
-    PRIMARY KEY (post_id, user_id),
-    FOREIGN Key (post_id) references post(id),
-    FOREIGN Key (user_id) references member(id)
+  post_id       INT,
+  user_id       INT,
+  reaction_type INT,
+  PRIMARY KEY (post_id, user_id),
+  FOREIGN KEY (post_id) REFERENCES post (id),
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS comment CASCADE;
 CREATE TABLE comment (
-    id int PRIMARY KEY,
-    post_id int,
-    user_id int,
-    comment_text varchar,
-    created_at TIMESTAMP,
-    update_time TIMESTAMP,
-    FOREIGN Key (user_id) references member(id),
-    FOREIGN Key (post_id) references post(id)
+  id           INT PRIMARY KEY,
+  post_id      INT,
+  user_id      INT,
+  comment_text VARCHAR,
+  created_at   TIMESTAMP,
+  update_time  TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES member (id),
+  FOREIGN KEY (post_id) REFERENCES post (id)
 
 );
 
 DROP TABLE IF EXISTS comment_tags CASCADE;
 CREATE TABLE comment_tags (
-    comment_id int,
-    user1_id int,
-    user2_id int,
-    PRIMARY KEY (comment_id, user1_id, user2_id),
-    FOREIGN Key (comment_id) references comment(id),
-    FOREIGN Key (user1_id) references member(id),
-    FOREIGN Key (user2_id) references member(id)
+  comment_id INT,
+  user1_id   INT,
+  user2_id   INT,
+  PRIMARY KEY (comment_id, user1_id, user2_id),
+  FOREIGN KEY (comment_id) REFERENCES comment (id),
+  FOREIGN KEY (user1_id) REFERENCES member (id),
+  FOREIGN KEY (user2_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS comment_likes CASCADE;
 CREATE TABLE comment_likes (
-    comment_id int,
-    user_id int,
-    PRIMARY KEY (comment_id, user_id),
-    FOREIGN Key (comment_id) references comment(id),
-    FOREIGN Key (user_id) references member(id)
+  comment_id INT,
+  user_id    INT,
+  PRIMARY KEY (comment_id, user_id),
+  FOREIGN KEY (comment_id) REFERENCES comment (id),
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS message_thread CASCADE;
 CREATE TABLE message_thread (
-    id Serial PRIMARY KEY,
-    created_at TIMESTAMP
+  id         SERIAL PRIMARY KEY,
+  created_at TIMESTAMP
 );
 
 DROP TABLE IF EXISTS message CASCADE;
 CREATE TABLE message (
-    id SErial PRIMARY KEY,
-    thread_id int,
-    user_id int,
-    message_text varchar,
-    created_at TIMESTAMP,
-    FOREIGN Key (thread_id) references message_thread(id),
-    FOREIGN Key (user_id) references member(id)
+  id           SERIAL PRIMARY KEY,
+  thread_id    INT,
+  user_id      INT,
+  message_text VARCHAR,
+  created_at   TIMESTAMP,
+  FOREIGN KEY (thread_id) REFERENCES message_thread (id),
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS message_thread_users CASCADE;
 CREATE TABLE message_thread_users (
-    thread_id int,
-    user_id int,
-    PRIMARY KEY (thread_id, user_id),
-    FOREIGN Key (thread_id) references message_thread(id),
-    FOREIGN Key (user_id) references member(id)
+  thread_id INT,
+  user_id   INT,
+  PRIMARY KEY (thread_id, user_id),
+  FOREIGN KEY (thread_id) REFERENCES message_thread (id),
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS event CASCADE;
 CREATE TABLE event (
-    id int PRIMARY KEY,
-    title varchar,
-    description varchar,
-    start_date TIMESTAMP,
-    end_date TIMESTAMP,
-    creator_id int,
-    created_at TIMESTAMP,
-    FOREIGN Key (creator_id) references member(id)
+  id          INT PRIMARY KEY,
+  title       VARCHAR,
+  description VARCHAR,
+  start_date  TIMESTAMP,
+  end_date    TIMESTAMP,
+  creator_id  INT,
+  created_at  TIMESTAMP,
+  FOREIGN KEY (creator_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS event_subscribers CASCADE;
 CREATE TABLE event_subscribers (
-    event_id int,
-    user_id int,
-    attending boolean DEFAULT NULL,
-    PRIMARY KEY (event_id, user_id),
-    FOREIGN Key (event_id) references event(id),
-    FOREIGN Key (user_id) references member(id)
+  event_id  INT,
+  user_id   INT,
+  attending BOOLEAN DEFAULT NULL,
+  PRIMARY KEY (event_id, user_id),
+  FOREIGN KEY (event_id) REFERENCES event (id),
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS image CASCADE;
 CREATE TABLE image (
-    id serial PRIMARY KEY,
-    image_path varchar
+  id         SERIAL PRIMARY KEY,
+  image_path VARCHAR
 );
 
 DROP TABLE IF EXISTS saved_posts CASCADE;
 CREATE TABLE saved_posts (
-    post_id int,
-    user_id int,
-    PRIMARY KEY (post_id, user_id),
-    FOREIGN Key (post_id) references post(id),
-    FOREIGN Key (user_id) references member(id)
+  post_id INT,
+  user_id INT,
+  PRIMARY KEY (post_id, user_id),
+  FOREIGN KEY (post_id) REFERENCES post (id),
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS facebook_group CASCADE;
 CREATE TABLE facebook_group (
-    id int PRIMARY KEY,
-    name varchar,
-    description varchar,
-    creator_id int,
-    created_at TIMESTAMP
+  id          INT PRIMARY KEY,
+  name        VARCHAR,
+  description VARCHAR,
+  creator_id  INT,
+  created_at  TIMESTAMP
 );
 
 DROP TABLE IF EXISTS group_members CASCADE;
 CREATE TABLE group_members (
-    group_id int,
-    user_id i,t,
-    admin boolean,
-    PRIMARY KEY (group_id, user_id),
-    FOREIGN Key (group_id) references facebook_group(id),
-    FOREIGN Key (user_id) references member(id)
+  group_id INT,
+  user_id  i,
+  t,
+  admin    BOOLEAN,
+  PRIMARY KEY (group_id, user_id),
+  FOREIGN KEY (group_id) REFERENCES facebook_group (id),
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS group_post CASCADE;
 CREATE TABLE group_post (
-    id serial PRIMARY KEY,
-    group_id int,
-    user_id int,
-    post_text varchar,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    FOREIGN Key (group_id) references facebook_group(id),
-    FOREIGN Key (user_id) references member(id)
+  id         SERIAL PRIMARY KEY,
+  group_id   INT,
+  user_id    INT,
+  post_text  VARCHAR,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  FOREIGN KEY (group_id) REFERENCES facebook_group (id),
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS group_post_comments CASCADE;
 CREATE TABLE group_post_comments (
-    id serial PRIMARY KEY,
-    group_id int,
-    group_post_id int,
-    user_id int,
-    comment_text varchar,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    FOREIGN Key (group_id) references facebook_group(id),
-    FOREIGN Key (user_id) references member(id),
-    FOREIGN KEY (post_id) references group_post(id)
+  id            SERIAL PRIMARY KEY,
+  group_id      INT,
+  group_post_id INT,
+  user_id       INT,
+  comment_text  VARCHAR,
+  created_at    TIMESTAMP,
+  updated_at    TIMESTAMP,
+  FOREIGN KEY (group_id) REFERENCES facebook_group (id),
+  FOREIGN KEY (user_id) REFERENCES member (id),
+  FOREIGN KEY (post_id) REFERENCES group_post (id)
 );
 
 DROP TABLE IF EXISTS follows CASCADE;
 CREATE TABLE follows (
-    user1_id int,
-    user2_id int,
-    PRIMARY Key (user1_id, user2_id),
-    FOREIGN Key (user1_id) references member(id),
-    FOREIGN Key (user2_id) references member(id)
+  user1_id INT,
+  user2_id INT,
+  PRIMARY KEY (user1_id, user2_id),
+  FOREIGN KEY (user1_id) REFERENCES member (id),
+  FOREIGN KEY (user2_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS comment_replies CASCADE;
 CREATE TABLE comment_replies (
-    id serial PRIMARY KEY,
-    comment_id int,
-    user_id int,
-    reply_text varchar,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    FOREIGN Key (comment_id) references comment(id),
-    FOREIGN Key (user_id) references member(id)
+  id         SERIAL PRIMARY KEY,
+  comment_id INT,
+  user_id    INT,
+  reply_text VARCHAR,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  FOREIGN KEY (comment_id) REFERENCES comment (id),
+  FOREIGN KEY (user_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS report_user CASCADE;
 CREATE TABLE report_user (
-    user1_id int,
-    user2_id int,
-    PRIMARY KEY (user1_id, user2_id),
-    FOREIGN Key (user1_id) references member(id),
-    FOREIGN Key (user2_id) references member(id)
+  user1_id INT,
+  user2_id INT,
+  PRIMARY KEY (user1_id, user2_id),
+  FOREIGN KEY (user1_id) REFERENCES member (id),
+  FOREIGN KEY (user2_id) REFERENCES member (id)
 );
 
 DROP TABLE IF EXISTS report_post CASCADE;
 CREATE TABLE report_post (
-    user_id int,
-    post_id int,
-    PRIMARY KEY (user_id, post_id),
-    FOREIGN Key (user_id) references member(id),
-    FOREIGN Key (post_id) references post(id)
+  user_id INT,
+  post_id INT,
+  PRIMARY KEY (user_id, post_id),
+  FOREIGN KEY (user_id) REFERENCES member (id),
+  FOREIGN KEY (post_id) REFERENCES post (id)
 );
 
 DROP TABLE IF EXISTS report_comment CASCADE;
 CREATE TABLE report_comment (
-    user_id int,
-    comment_id int,
-    PRIMARY KEY (user_id, comment_id),
-    FOREIGN Key (user_id) references member(id),
-    FOREIGN Key (comment_id) references comment(id)
+  user_id    INT,
+  comment_id INT,
+  PRIMARY KEY (user_id, comment_id),
+  FOREIGN KEY (user_id) REFERENCES member (id),
+  FOREIGN KEY (comment_id) REFERENCES comment (id)
 );
 
 DROP TABLE IF EXISTS report_group CASCADE;
 CREATE TABLE report_group (
-    user_id int,
-    group_id int,
-    PRIMARY KEY (user_id, group_id),
-    FOREIGN Key (user_id) references member(id),
-    FOREIGN Key (group_id) references facebook_group(id)
+  user_id  INT,
+  group_id INT,
+  PRIMARY KEY (user_id, group_id),
+  FOREIGN KEY (user_id) REFERENCES member (id),
+  FOREIGN KEY (group_id) REFERENCES facebook_group (id)
 );
 
 DROP TABLE IF EXISTS user_album CASCADE;
 CREATE TABLE user_album (
-    user_id int,
-    picture_id int,
-    PRIMARY KEY (user_id, picture_id),
-    FOREIGN Key (user_id) references member(id),
-    FOREIGN Key (picture_id) references image(id)
+  user_id    INT,
+  picture_id INT,
+  PRIMARY KEY (user_id, picture_id),
+  FOREIGN KEY (user_id) REFERENCES member (id),
+  FOREIGN KEY (picture_id) REFERENCES image (id)
 );
 
 DROP TABLE IF EXISTS post_images CASCADE;
 CREATE TABLE post_images (
-    post_id int,
-    picture_id int,
-    PRIMARY KEY (post_id, picture_id),
-    FOREIGN Key (post_id) references post(id),
-    FOREIGN Key (picture_id) references image(id)
+  post_id    INT,
+  picture_id INT,
+  PRIMARY KEY (post_id, picture_id),
+  FOREIGN KEY (post_id) REFERENCES post (id),
+  FOREIGN KEY (picture_id) REFERENCES image (id)
 );
 
 DROP TABLE IF EXISTS comment_image CASCADE;
 CREATE TABLE comment_image (
-    comment_id int,
-    picture_id int,
-    PRIMARY KEY (comment_id, picture_id),
-    FOREIGN Key (comment_id) references comment(id),
-    FOREIGN Key (picture_id) references image(id)
+  comment_id INT,
+  picture_id INT,
+  PRIMARY KEY (comment_id, picture_id),
+  FOREIGN KEY (comment_id) REFERENCES comment (id),
+  FOREIGN KEY (picture_id) REFERENCES image (id)
 );
 
 DROP TABLE IF EXISTS reply_image CASCADE;
 CREATE TABLE reply_image (
-    reply_id int,
-    picture_id int,
-    PRIMARY KEY (reply_id, picture_id),
-    FOREIGN Key (reply_id) references comment_replies(id),
-    FOREIGN Key (picture_id) references image(id)
+  reply_id   INT,
+  picture_id INT,
+  PRIMARY KEY (reply_id, picture_id),
+  FOREIGN KEY (reply_id) REFERENCES comment_replies (id),
+  FOREIGN KEY (picture_id) REFERENCES image (id)
 );
--- database/user_procs.sql
+++ database/user_procs.sql
@@ -1,42 +1,45 @@
-CREATE OR REPLACE FUNCTION sign_up(email VARCHAR, hashed_password VARCHAR)
-RETURNS BOOLEAN AS $$  --Delimiter for functions and strings
-DECLARE userID    INTEGER;
+CREATE OR REPLACE FUNCTION sign_up(email VARCHAR, hashed_password VARCHAR, fName VARCHAR, lName VARCHAR, dob DATE)
+  RETURNS BOOLEAN AS $$  --Delimiter for functions and strings
+DECLARE userID INTEGER;
 BEGIN
-INSERT INTO Member (member_id, email, password_hash, creation_time, last_login)
-    VALUES (userID, email, hashed_password, creation_time, last_login);
+  INSERT INTO member VALUES (DEFAULT, email, hashed_password, fName, lName, dob, now() :: TIMESTAMP);
+  INSERT INTO users VALUES (CURRVAL(pg_get_serial_sequence('member', 'id')));
 
 END; $$
 LANGUAGE PLPGSQL;
 
-CREATE OR REPLACE FUNCTION login(member_id VARCHAR)
-RETURNS BOOLEAN AS $$  --Delimiter for functions and strings
+CREATE OR REPLACE FUNCTION login(member_id VARCHAR, session VARCHAR, address VARCHAR)
+  RETURNS BOOLEAN AS $$  --Delimiter for functions and strings
+DECLARE current_time TIMESTAMP := now() :: TIMESTAMP;
+
 BEGIN
-    UPDATE last_login
-    INTO Member
-    VALUES (now() :: TIMESTAMP)
-    WHERE id = member_id
+  INSERT INTO logins
+  VALUES (DEFAULT, member_id, current_time, address, FALSE);
+
+  INSERT INTO sessions
+  VALUES (DEFAULT, member_id, current_time, current_time, address, FALSE);
+
 END; $$
 LANGUAGE PLPGSQL;
 
-CREATE OR REPLACE FUNCTION edit_user(member_id VARCHAR,
-                                       email VARCHAR,
-                                       password VARCHAR,
-                                       first_name VARCHAR,
-                                       last_name VARCHAR,
-                                       date_of_birth DATE,
-                                       )
-    RETURNS SETOF users AS $$
+CREATE OR REPLACE FUNCTION edit_user(member_id         VARCHAR,
+                                     new_email         VARCHAR,
+                                     new_password      VARCHAR,
+                                     new_first_name    VARCHAR,
+                                     new_last_name     VARCHAR,
+                                     new_date_of_birth DATE)
+  RETURNS SETOF users AS $$
 BEGIN
-    INSERT INTO Member(email, password_hash, first_name, last_name, date_of_birth)
-    VALUES (email, password, first_name, last_name, date_of_birth)
-    WHERE id = member_id
+  UPDATE member
+  SET email = new_email, first_name = new_first_name, last_name = new_last_name, date_of_birth = new_date_of_birth
+  WHERE id = member_id;
 END; $$
 LANGUAGE PLPGSQL;
 
 CREATE OR REPLACE FUNCTION delete_user(member_id VARCHAR)
-    RETURNS VOID AS $$
+  RETURNS VOID AS $$
 BEGIN
-    DELETE FROM Member
-    WHERE id = member_id
+  DELETE FROM Member
+  WHERE id = member_id;
 END; $$
 LANGUAGE PLPGSQL;
