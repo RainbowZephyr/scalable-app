7a1ff42cc5034395471252053b61912975011783
RainbowZephyr <darkfeathers9@gmail.com>
2017-03-09 15:27:16 +0200

    pulling updates

--- database/user_procs.sql
+++ database/user_procs.sql
@@ -1,11 +1,10 @@
 CREATE OR REPLACE FUNCTION sign_up(email VARCHAR, hashed_password VARCHAR)
 RETURNS BOOLEAN AS $$  --Delimiter for functions and strings
 DECLARE userID    INTEGER;
-        creation_time CURRENT_TIMESTAMP;
-        last_login CURRENT_TIMESTAMP  ;
 BEGIN
 INSERT INTO Member (member_id, email, password_hash, creation_time, last_login)
-    VALUES (userID, email, hashed_password, creation_time, last_login)
+    VALUES (userID, email, hashed_password, creation_time, last_login);
+
 END; $$
 LANGUAGE PLPGSQL;
 
