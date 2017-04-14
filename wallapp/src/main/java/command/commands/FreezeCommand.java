40061781c2979e795fcee0ce65748b7d3bbd8f37
MostafaGohar <mostafa-gohar@hotmail.com>
2017-04-15 00:06:49 +0200

    updated response to admin commands

--- wallapp/src/main/java/command/commands/FreezeCommand.java
+++ wallapp/src/main/java/command/commands/FreezeCommand.java
@@ -3,6 +3,7 @@ package command.commands;
 import command.Command;
 import services.Response;
 import thread_pools.CommandsThreadPool;
+import utility.Constants;
 import utility.ResponseCodes;
 
 import java.util.Map;
@@ -12,6 +13,8 @@ public class FreezeCommand extends Command {
     public StringBuffer execute(Map<String, Object> requestMapData) throws Exception {
         CommandsThreadPool.sharedInstance().getThreadPool().shutdown();
         Response response = new Response(ResponseCodes.STATUS_OK);
+        response.addToResponse("app_id", Constants.APPLICATION_ID);
+        response.addToResponse("service_type", "freeze");
         return response.toJson();
     }
 }
