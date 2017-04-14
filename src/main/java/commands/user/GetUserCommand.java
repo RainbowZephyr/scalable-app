b6176b1d1afcf0594830eb993271405b649e30ea
AmiraElEwady <amiramelewady@gmail.com>
2017-04-14 14:10:10 +0200

    added command

--- src/main/java/commands/user/GetUserCommand.java
+++ src/main/java/commands/user/GetUserCommand.java
@@ -3,28 +3,30 @@ package commands.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import command.Command;
+
 import services.RequestHandle;
 import thread_pools.DatabaseThreadPool;
-import command.Command;
 import datastore.DataStoreConnection;
 import datastore.DataStoreConnectionFactory;
 
-public class LoginCommand extends Command {
+public class GetUserCommand extends Command{
 
-	@Override
+	
+	
 	protected StringBuffer execute(Map<String, Object> requestMapData)
 			throws Exception {
 
-		String email = (String)requestMapData.get("email");
-		String password = (String)requestMapData.get("password");
+		int userID = (Integer) requestMapData.get("userID");
+		
 		Class<?> connectionClass = DataStoreConnectionFactory.sharedInstance()
 		.getDataStoreConnection("Postgresql_database_connection");
 		DataStoreConnection connection = (DataStoreConnection) connectionClass.newInstance();
 		
 		Map<String, Object> parameters = new HashMap<String, Object>();
-		parameters.put("action", "loginUser");
-		parameters.put("email", email);
-		parameters.put("password", password);
+		parameters.put("action", "getUser");
+		parameters.put("email", userID);
+	
 		RequestHandle requestHandle = (RequestHandle)
 		this.parameters.get(RequestHandle.class.getSimpleName());
 		parameters.put(RequestHandle.class.getSimpleName(), requestHandle);
@@ -34,5 +36,8 @@ public class LoginCommand extends Command {
 		
 		return null;
 	}
-
+	
+	
+	
+	
 }
