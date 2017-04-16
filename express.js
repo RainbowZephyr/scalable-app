ac1f1cc5a41ea3950903955a1e0f074f28eb1f1d
RainbowZephyr <darkfeathers9@gmail.com>
2017-04-16 22:54:27 +0200

    added Expires header to Express to handle caching

--- express.js
+++ express.js
@@ -3,10 +3,13 @@ var app = express()
 var timeInMs = Date.now();
 
 app.get('/', function (req, res) {
-    res.send('Date now in Ms:'+Date.now())
+    res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
+    console.log(res);
+
+    res.send('HELLO');
     console.log("Received on 3000")
-})
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
-})
+});
--- express2.js
+++ express2.js
@@ -3,10 +3,12 @@ var app = express()
 var timeInMs = Date.now();
 
 app.get('/', function (req, res) {
-    res.send('Date in Ms'+Date.now())
-    console.log("Received on 4000")
-})
+    res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
+    console.log(res);
+    res.send('HELLO');
+    console.log("Received on 4000");
+});
 
 app.listen(4000, function () {
     console.log('Example app listening on port 4000!')
-})
+});
