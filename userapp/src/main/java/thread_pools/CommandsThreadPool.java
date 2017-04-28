fb788379bec0eaf3a524c1bd6a3c93fa2ed04d77
abdoofathy95 <abdoofathy95@gmail.com>
2017-04-28 12:56:27 +0300

    Update CommandsThreadPool.java

--- userapp/src/main/java/thread_pools/CommandsThreadPool.java
+++ userapp/src/main/java/thread_pools/CommandsThreadPool.java
@@ -1,5 +1,6 @@
 package thread_pools;
-import java.util.concurrent.LinkedBlockingDeque;
+
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -17,9 +18,7 @@ public class CommandsThreadPool{
                     DEFAULT_POOL_SIZE,
                     0,
                     TimeUnit.NANOSECONDS,
-                    retryEvery,
-                    retryEveryTimeUnit,
-                    new LinkedBlockingDeque<Runnable>());
+                    new SynchronousQueue<Runnable>()); // Queue that holds nothing
 
     private CommandsThreadPool() {
     }
@@ -38,23 +37,11 @@ public class CommandsThreadPool{
                 poolSize,
                 threadPool.getKeepAliveTime(TimeUnit.NANOSECONDS),
                 TimeUnit.NANOSECONDS,
-                retryEvery,
-                retryEveryTimeUnit,
                 threadPool.getQueue());
     }
 
     public void setMaxThreadPoolSize(int size) {
         poolSize = size;
         getThreadPool().setCorePoolSize(poolSize);
-    }
-
-    /**
-     * set
-     * @param retryEvery - units of timeUnit to wait till resubmitting a task
-     * @param timeUnit - TimeUnit (nano, milliseconds, seconds)
-     */
-    public void setRetryEvery(long retryEvery, TimeUnit timeUnit){
-        this.retryEvery = retryEvery;
-        this.retryEveryTimeUnit = timeUnit;
     }
 }
